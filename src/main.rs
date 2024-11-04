96f3086b57cdc30cc771aeb7f51a2d636600a44d
priyanshu shrivastava <99priyanshu33@gmail.com>
2024-11-04 22:59:23 +0530

    moving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let mut s1 :String = String::from("harik");
+    let mut s2 = s1;
+    s1 =s2.clone();
+    println!("{}",s1);
+    println!("{}",s2);
 }
